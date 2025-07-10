361f6a0c29cceb083ece5f78a75e0554012ddd65
Zichun Yu <yuzc19@gmail.com>
2025-07-10 12:12:57 -0400

    Change path

--- src/infer/launch.sh
+++ src/infer/launch.sh
@@ -3,7 +3,7 @@ end=$2
 gpu_index=0
 for ((s=begin; s<=end; s++)); do
     echo $s
-    PYTHONPATH=$PWD/src CUDA_VISIBLE_DEVICES=$gpu_index python run_infer.py $s \
+    PYTHONPATH=$PWD/src CUDA_VISIBLE_DEVICES=$gpu_index python src/infer/run_infer.py $s \
     > logs/log_job_s${s}_gpu${gpu_index}.out 2>&1 &
     ((gpu_index=(gpu_index+1)%8))
 done
